ad8396dd5149c7d13f3eb71faf44a9e157dfe8cb
Adam Ruka <adamruka85@gmail.com>
2016-12-11 23:52:04 +0100

    Generate an empty Builder class from the annotated class.

--- src/main/java/org/juilt/Builder.java
+++ src/main/java/org/juilt/Builder.java
@@ -1,4 +1,8 @@
 package org.juilt;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
 public @interface Builder {
 }
--- src/main/java/org/juilt/JuiltAnnotationProcessor.java
+++ src/main/java/org/juilt/JuiltAnnotationProcessor.java
@@ -1,29 +1,69 @@
 package org.juilt;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
+
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Set;
 
 public class JuiltAnnotationProcessor extends AbstractProcessor {
     private Messager messager;
+    private Filer filer;
+    private Elements elements;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         messager = processingEnv.getMessager();
+        filer = processingEnv.getFiler();
+        elements = processingEnv.getElementUtils();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        messager.printMessage(Diagnostic.Kind.NOTE, "The Juilt processor was called!");
+        for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(Builder.class)) {
+            if (annotatedElement.getKind() != ElementKind.CLASS) {
+                return error(annotatedElement, "Only classes can be annotated with @%s",
+                        Builder.class.getName());
+            }
 
-        return false;
+            TypeElement typeElement = (TypeElement) annotatedElement;
+            String builderClassName = typeElement.getSimpleName() + "Builder";
+
+            TypeSpec typeSpec = TypeSpec.classBuilder(builderClassName)
+                    .addModifiers(Modifier.PUBLIC)
+                    .build();
+
+            JavaFile javaFile = JavaFile
+                    .builder(elements.getPackageOf(typeElement).toString(), typeSpec)
+                    .build();
+
+            try {
+                javaFile.writeTo(filer);
+            } catch (IOException e) {
+                return error(null, e.getMessage());
+            }
+        }
+
+        return true;
+    }
+
+    private boolean error(Element element, String msg, Object... args) {
+        messager.printMessage(Diagnostic.Kind.ERROR, String.format(msg, args), element);
+        return true;
     }
 
     @Override
