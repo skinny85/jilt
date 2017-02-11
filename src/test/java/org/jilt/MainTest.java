220bf411816dc40dca49bc74c5c7e1031d015606
Adam Ruka <adamruka85@gmail.com>
2017-02-11 13:45:18 +0100

    Change the Gradle build settings to generate the annotation processor files in src/main/generated/java and src/test/generated/java.

---
+++ src/test/java/org/jilt/MainTest.java
@@ -0,0 +1,10 @@
+package org.jilt;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void test_builder_class_generation() throws Exception {
+        new TestClassBuilder();
+    }
+}
