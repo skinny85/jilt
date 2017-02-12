ff1ed1ae8313e682269567f87dde93fced8b7c0b
Adam Ruka <adamruka85@gmail.com>
2017-02-12 19:05:18 +0100

    Change the retention policy of the Builder annotation to Source.

--- src/main/java/org/jilt/Builder.java
+++ src/main/java/org/jilt/Builder.java
@@ -1,9 +1,12 @@
 package org.jilt;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Builder {
     BuilderVariant variant() default BuilderVariant.CLASSIC;
 
