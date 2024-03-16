48c06b293cfb4f7a2d2aacc10c90458ab7df1306
Adam Ruka <adamruka85@gmail.com>
2024-03-16 10:29:40 -0700

    Small refactoring - change two fields in JiltAnnotationProcessor to local variables.

--- src/main/java/org/jilt/JiltAnnotationProcessor.java
+++ src/main/java/org/jilt/JiltAnnotationProcessor.java
@@ -3,32 +3,27 @@ package org.jilt;
 import org.jilt.internal.BuilderGeneratorFactory;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
 import java.util.Collections;
 import java.util.Set;
 
 public class JiltAnnotationProcessor extends AbstractProcessor {
     private Messager messager;
-    private Filer filer;
-    private Elements elements;
     private BuilderGeneratorFactory builderGeneratorFactory;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
 
-        messager = processingEnv.getMessager();
-        filer = processingEnv.getFiler();
-        elements = processingEnv.getElementUtils();
-        builderGeneratorFactory = new BuilderGeneratorFactory(filer, elements);
+        this.messager = processingEnv.getMessager();
+        this.builderGeneratorFactory = new BuilderGeneratorFactory(
+                processingEnv.getFiler(), processingEnv.getElementUtils());
     }
 
     @Override
