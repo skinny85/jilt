6598fde921e1fe510fdfb42bfc6dab9e4ea777d8
Diego Pedregal <diego.p.hidalgo@gmail.com>
2024-03-26 09:07:51 +0100

    Add @Documented

--- src/main/java/org/jilt/BuilderInterfaces.java
+++ src/main/java/org/jilt/BuilderInterfaces.java
@@ -1,5 +1,6 @@
 package org.jilt;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -25,6 +26,7 @@ import java.lang.annotation.Target;
  * @see Builder#style
  * @see BuilderStyle
  */
+@Documented
 @Target({ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.SOURCE)
 public @interface BuilderInterfaces {
