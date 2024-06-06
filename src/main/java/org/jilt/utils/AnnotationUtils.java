252cd3b727d6d7a0083623ce2089ebc5e4392bd1
Diego Pedregal <diego.p.hidalgo@gmail.com>
2024-06-06 11:33:13 +0200

    Fix backwards compatibility issue

--- src/main/java/org/jilt/utils/AnnotationUtils.java
+++ src/main/java/org/jilt/utils/AnnotationUtils.java
@@ -3,12 +3,21 @@ package org.jilt.utils;
 import javax.lang.model.element.AnnotationMirror;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.util.EnumSet;
 import java.util.Set;
 
 public final class AnnotationUtils {
-    private static final Set<ElementType> NON_PARAMETER_ANNOTATIONS = Set.of(ElementType.TYPE, ElementType.FIELD,
+    private static final Set<ElementType> NON_PARAMETER_ANNOTATIONS = EnumSet.of(ElementType.TYPE, ElementType.FIELD,
             ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.LOCAL_VARIABLE, ElementType.ANNOTATION_TYPE,
-            ElementType.PACKAGE, ElementType.TYPE_PARAMETER, ElementType.MODULE, ElementType.RECORD_COMPONENT);
+            ElementType.PACKAGE, ElementType.TYPE_PARAMETER, ElementType.MODULE);
+
+    static {
+        try {
+            NON_PARAMETER_ANNOTATIONS.add((ElementType) ElementType.class.getField("RECORD_COMPONENT").get(null));
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            // Java version does not support RECORD_COMPONENT
+        }
+    }
 
     private AnnotationUtils() {
     }
