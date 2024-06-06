6c730c2f8583862794f6e57ac6fc369cf086162b
Diego Pedregal <diego.p.hidalgo@gmail.com>
2024-06-06 11:46:37 +0200

    Remove MODULE and RECORD_COMPONENT from annotation target

--- src/test/java/org/jilt/test/data/annotations/NonParameterAnnotation.java
+++ src/test/java/org/jilt/test/data/annotations/NonParameterAnnotation.java
@@ -4,7 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.LOCAL_VARIABLE,
-        ElementType.ANNOTATION_TYPE, ElementType.PACKAGE, ElementType.TYPE_PARAMETER, ElementType.MODULE,
-        ElementType.RECORD_COMPONENT})
+        ElementType.ANNOTATION_TYPE, ElementType.PACKAGE, ElementType.TYPE_PARAMETER})
 public @interface NonParameterAnnotation {
 }
