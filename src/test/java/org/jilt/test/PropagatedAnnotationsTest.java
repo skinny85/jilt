db58ff5f8b53c0df1d712134cfc43a3e2af9188f
Diego Pedregal <43535391+thmasker@users.noreply.github.com>
2024-06-08 09:29:45 +0200

    Fix tests

--- src/test/java/org/jilt/test/PropagatedAnnotationsTest.java
+++ src/test/java/org/jilt/test/PropagatedAnnotationsTest.java
@@ -15,26 +15,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PropagatedAnnotationsTest {
     @Test
     public void setter_in_builder_propagates_annotations() throws Exception {
-        Method firstNameSetter = PropagatedAnnotationNameBuilder.class.getMethod("firstName", String.class);
+        Method firstNameSetter = PropagatedAnnotationsFullNameBuilder.class.getMethod("firstName", String.class);
         assertThat(firstNameSetter.getParameters()[0].getDeclaredAnnotation(CheckForNull.class)).isNotNull();
 
-        Method middleNameSetter = PropagatedAnnotationNameBuilder.class.getMethod("middleName", String.class);
+        Method middleNameSetter = PropagatedAnnotationsFullNameBuilder.class.getMethod("middleName", String.class);
         assertThat(middleNameSetter.getParameters()[0].getDeclaredAnnotation(Nullable.class)).isNotNull();
 
-        Method lastNameSetter = PropagatedAnnotationNameBuilder.class.getMethod("lastName", String.class);
+        Method lastNameSetter = PropagatedAnnotationsFullNameBuilder.class.getMethod("lastName", String.class);
         assertThat(lastNameSetter.getParameters()[0].getDeclaredAnnotation(Nonnull.class)).isNotNull();
         assertThat(lastNameSetter.getParameters()[0].getDeclaredAnnotation(NonParameterAnnotation.class)).isNull();
     }
 
     @Test
     public void setter_in_type_safe_interface_propagates_annotations() throws Exception {
-        Method firstNameSetter = PropagatedAnnotationNameBuilders.FirstName.class.getMethod("firstName", String.class);
+        Method firstNameSetter = PropagatedAnnotationsFullNameBuilders.FirstName.class.getMethod("firstName", String.class);
         assertThat(firstNameSetter.getParameters()[0].getDeclaredAnnotation(CheckForNull.class)).isNotNull();
 
-        Method middleNameSetter = PropagatedAnnotationNameBuilders.MiddleName.class.getMethod("middleName", String.class);
+        Method middleNameSetter = PropagatedAnnotationsFullNameBuilders.MiddleName.class.getMethod("middleName", String.class);
         assertThat(middleNameSetter.getParameters()[0].getDeclaredAnnotation(Nullable.class)).isNotNull();
 
-        Method lastNameSetter = PropagatedAnnotationNameBuilders.LastName.class.getMethod("lastName", String.class);
+        Method lastNameSetter = PropagatedAnnotationsFullNameBuilders.LastName.class.getMethod("lastName", String.class);
         assertThat(lastNameSetter.getParameters()[0].getDeclaredAnnotation(Nonnull.class)).isNotNull();
         assertThat(lastNameSetter.getParameters()[0].getDeclaredAnnotation(NonParameterAnnotation.class)).isNull();
     }
