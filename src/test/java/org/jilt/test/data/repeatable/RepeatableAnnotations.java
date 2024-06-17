29e7a250eeb234400d2efe53e3bf7a5915327303
Diego Pedregal <diego.p.hidalgo@gmail.com>
2024-06-17 12:45:27 +0200

    Remove Java <8 incompatible tests

--- src/test/java/org/jilt/test/data/repeatable/RepeatableAnnotations.java
+++
@@ -1,9 +0,0 @@
-package org.jilt.test.data.repeatable;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface RepeatableAnnotations {
-    RepeatableAnnotation[] value();
-}
