ada01cd38efd343f3003e92aa713f5e1eab77578
David Olmos <davidog23@users.noreply.github.com>
2024-12-21 08:50:03 +0100

    Use correct @Generated annotation when compiling with `--release 8` (#34)
    `@javax.annotation.processing.Generated` is not available for compilation when passing
    `--release 8` to the compiler,
    but it is on the classpath. So, checking for its availability with `Class.forName()`,
    like Jilt does currently, will return the wrong result.
    
    The solution is to use the `Elements` object to find the `@Generated` class,
    which respects the `--release 8` compiler argument,
    and returns `null` for `@javax.annotation.processing.Generated`
    when that option is set.

--- src/main/java/org/jilt/internal/AbstractBuilderGenerator.java
+++ src/main/java/org/jilt/internal/AbstractBuilderGenerator.java
@@ -493,20 +493,18 @@ abstract class AbstractBuilderGenerator implements BuilderGenerator {
         return this.targetCreationMethod.getModifiers().contains(Modifier.PRIVATE);
     }
 
-    protected final AnnotationSpec generatedAnnotation() throws Exception {
-        Class<?> generatedAnnotationClass = determineGeneratedAnnotationClass();
+    protected final AnnotationSpec generatedAnnotation() {
+        ClassName generatedAnnotationClass = determineGeneratedAnnotationClass();
         return AnnotationSpec
                 .builder(generatedAnnotationClass)
                 .addMember("value", "$S", "Jilt-1.6.2")
                 .build();
     }
 
-    private Class<?> determineGeneratedAnnotationClass() throws Exception {
-        try {
-            // available since 9
-            return Class.forName("javax.annotation.processing.Generated");
-        } catch (ClassNotFoundException e) {
-            return Class.forName("javax.annotation.Generated");
-        }
+    private ClassName determineGeneratedAnnotationClass() {
+        TypeElement generatedAnnotation = this.elements.getTypeElement("javax.annotation.processing.Generated");
+        return ClassName.get(generatedAnnotation == null
+            ? this.elements.getTypeElement("javax.annotation.Generated")
+            : generatedAnnotation);
     }
 }
