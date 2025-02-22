190cbee79d5043c5999fe92eaef565a48aafcf9f
Adam Ruka <adamruka85@gmail.com>
2025-02-22 10:50:22 -0800

    Add unwrapping of Jetbrains ProcessingEnv wrapper

--- src/main/java/org/jilt/internal/LazyTrees.java
+++ src/main/java/org/jilt/internal/LazyTrees.java
@@ -3,6 +3,7 @@ package org.jilt.internal;
 import com.sun.source.util.TreePath;
 import com.sun.source.util.Trees;
 
+import java.lang.reflect.Method;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.VariableElement;
 
@@ -20,8 +21,20 @@ public final class LazyTrees {
 
     private Trees getTrees() {
         if (this.trees == null) {
-            this.trees = Trees.instance(this.processingEnv);
+            this.trees = Trees.instance(
+                    unwrapJetbrainsProcessingEnv(this.processingEnv));
         }
         return this.trees;
+    }
+
+    private static ProcessingEnvironment unwrapJetbrainsProcessingEnv(ProcessingEnvironment wrapped) {
+        ProcessingEnvironment unwrapped = null;
+        try {
+            Class<?> apiWrappers = wrapped.getClass().getClassLoader().loadClass("org.jetbrains.jps.javac.APIWrappers");
+            Method unwrapMethod = apiWrappers.getDeclaredMethod("unwrap", Class.class, Object.class);
+            unwrapped = (ProcessingEnvironment) unwrapMethod.invoke(null, ProcessingEnvironment.class, wrapped);
+        } catch (Exception ignored) {
+        }
+        return unwrapped == null? wrapped : unwrapped;
     }
 }
